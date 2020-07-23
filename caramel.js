4236a7265cb46667b61d1e99be30c2159f270902
Kevin Bluer <kevin@bluer.com>
2020-07-23 17:08:59 -0500

    basic artifact pinning functionality

--- caramel.js
+++ caramel.js
@@ -1,12 +1,11 @@
-/**
- * Outputs `Hello, World!` when running `truffle run hello`,
- * or `Hello, ${name}` when running `truffle run hello [name]`
- * @param {Config} config - A truffle-config object.
- * Has attributes like `truffle_directory`, `working_directory`, etc.
- */
+const Provider = require("@truffle/provider");
+const Web3 = require("web3");
+const pinataSDK = require('@pinata/sdk');
 
-var Provider = require("@truffle/provider");
-var Web3 = require("web3");
+require("dotenv").config();
+
+const pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_API_SECRET);
+const gatewayUri = `https://gateway.pinata.cloud/ipfs/`;
 
 module.exports = async (config) => {
   // config._ has the command arguments.
@@ -17,13 +16,26 @@ module.exports = async (config) => {
     return;
   }
 
-  const endpoint = config.networks.teams.url;
-  console.log(config.networks.teams.url);
+  const sourcePath = config.contracts_build_directory;
+  console.log(`Pinning from ${sourcePath}`);
 
-  const provider = Provider.create({
-    provider: new Web3.providers.HttpProvider(endpoint),
-  });
-
-  let name = config._.length > 1 ? config._[1] : 'Caramel!';
-  console.log(`Hello, ${name}`);
+  try {
+    const fs = require('fs');
+    const options = {
+        pinataMetadata: {
+            name: "meta",
+            keyvalues: {
+                candyName: 'caramel'
+            }
+        },
+        pinataOptions: {
+            cidVersion: 0
+        }
+    };
+    const result = await pinata.pinFromFS(sourcePath, options);
+    console.log(`Your assets were successfully pinned:\n\n${JSON.stringify(result, null, 2)}\n`);
+    console.log(`View your assets at the follow: ${gatewayUri}${result.IpfsHash}`);
+  } catch (err) {
+      console.log(`An error occurred when attempting to pin:\n\n${JSON.stringify(err), null, 2}`);
+  }
 }
